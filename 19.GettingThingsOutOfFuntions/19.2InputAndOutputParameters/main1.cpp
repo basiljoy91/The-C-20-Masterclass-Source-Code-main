e4ae226fde4c1585d5d3f570759097da6676e232
basiljoy91 <basiljoygreen@gmail.com>
2025-06-23 22:06:50 +0530

    pointer

--- 19.GettingThingsOutOfFuntions/19.2InputAndOutputParameters/main1.cpp
+++ 19.GettingThingsOutOfFuntions/19.2InputAndOutputParameters/main1.cpp
@@ -3,9 +3,11 @@
 
 using namespace std;
 
-void max_string(string &input1, string &input2, string &output ){
-    if (input1 > input2)
-        output = input1;
+void max_string(string &string1, string &input2, string &output ){
+    cout << &string1 <<  &input2 <<  &output << endl;
+
+    if (string1 > input2)
+        output = string1;
     else 
         output = input2;
 }
@@ -19,4 +21,7 @@ int main(){
     max_string(string1, string2, out_string);
 
     cout << "max_str: " << out_string << endl;
+
+    cout << &out_string << &string1 << &string2 << endl;
+
 }
