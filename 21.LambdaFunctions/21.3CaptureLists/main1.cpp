3f9fca7a94fb1a56b49025421f6842ad6f178372
basiljoy91 <basiljoygreen@gmail.com>
2025-07-06 11:56:20 +0530

    flambda caputre

---
+++ 21.LambdaFunctions/21.3CaptureLists/main1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+
+int main(){
+    
+   int c = 10;
+   
+   auto fun_1 = [c](){
+        cout << "inner_value: " << c << endl;
+   };
+
+    for (int i=0; i<=5; i++ ){
+        cout << "Outer_value: " << c << endl;
+        fun_1();
+        c++;
+    }
+
+    return 0;
+}
