4c8d349c2505d20f550933a3c7896c32e79f1ba0
basiljoy91 <basiljoygreen@gmail.com>
2026-01-24 19:39:03 +0530

    member_class_editing

---
+++ 26.Classes/26.2YourFirstClass/main_1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+const double PI = {3.14};
+class Cylinder{
+    public: 
+        double volume(){
+            return PI * radius * radius * height;
+        }
+
+        double radius = {1};
+        double height = {1};
+};
+
+int main(){
+
+    Cylinder Cylinder1;
+
+    Cylinder1.height = {4};
+    Cylinder1.radius = {2};
+    cout << "volume: " << Cylinder1.volume() << endl;
+    cout << "raduis: " << Cylinder1.radius << endl;
+    cout << "height: " << Cylinder1.height << endl;
+
+    return 0;
+
+}
