ea7674f32251a6dafdf6d8743267d6815ad050c5
basiljoy91 <basiljoygreen@gmail.com>
2026-01-24 23:52:51 +0530

    class_concepts

--- 26.Classes/26.3Constructors/main.cpp
+++ 26.Classes/26.3Constructors/main.cpp
@@ -1,36 +1,33 @@
-#include <iostream>
+#include<iostream>
 
-const double PI {3.1415926535897932384626433832795};
+using namespace std;
 
-class Cylinder {
-    public : 
-        //Constctors
+double PI = {3.14};
+
+class Cylinder{
+    private:
+
+        //member variables
+        double radius = {2};
+        double height = {5};
+    
+    public:
+        //constructor
         Cylinder(){
-            base_radius = 2.0;
-            height = 2.0;
+            radius = 1;
+            height = 2;
         }
 
-        Cylinder(double rad_param,double height_param){
-            base_radius = rad_param;
-            height = height_param;
-        }
-   
-        //Functions (methods)
+        //member function or methords
         double volume(){
-            return PI * base_radius * base_radius * height;
-        }
-
-    private : 
-        //Member variables
-        double base_radius{1};
-        double height{1};
+            return PI * radius * radius * height;
+        }  
 };
 
-
 int main(){
-    //Cylinder cylinder1(10,4); // Object
-    Cylinder cylinder1;
-    std::cout << "volume : " << cylinder1.volume() << std::endl;
-
+    
+    Cylinder cylinder1; //object
+    cout << "result: " << cylinder1.volume() << endl;
+    
     return 0;
 }
