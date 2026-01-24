e3cfd18791c0cc7c6869d30bb8bef6f0b12f1a15
basiljoy91 <basiljoygreen@gmail.com>
2026-01-24 17:20:16 +0530

    classes

--- 26.Classes/26.2YourFirstClass/main.cpp
+++ 26.Classes/26.2YourFirstClass/main.cpp
@@ -1,35 +1,25 @@
-#include <iostream>
+#include<iostream>
 
-const double PI {3.1415926535897932384626433832795};
+using namespace std;
 
-class Cylinder {
-    public : 
-        //Functions (methods)
+const double PI = {3.14};
+class Cylinder{
+    public: 
         double volume(){
-            return PI * base_radius * base_radius * height;
+            return PI * radius * radius * height;
         }
 
-    public : 
-        //Member variables
-        double base_radius{1};
-        double height{1};
+        double radius = {1};
+        double height = {1};
 };
-
 
 int main(){
 
-    Cylinder cylinder1; // Objects
-    std::cout << "volume : " << cylinder1.volume() << std::endl;
+    Cylinder Cylinder1;
+    cout << "volume: " << Cylinder1.volume() << endl;
+    cout << "raduis: " << Cylinder1.radius << endl;
+    cout << "height: " << Cylinder1.height << endl;
 
-    //Change the member variables
-    cylinder1.base_radius = 10;
-    cylinder1.height = 3;
-
-    std::cout << "volume : " << cylinder1.volume() << std::endl;
-
-    cylinder1.height =8;
-
-    std::cout << "volume : " << cylinder1.volume() << std::endl;
-   
     return 0;
+
 }
