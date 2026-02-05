9de285bfd6ac593c1432c1e6d9778cbbd282e177
basiljoy91 <basiljoygreen@gmail.com>
2026-02-05 15:12:12 +0530

    getters and setters

--- 26.Classes/26.5SettersAndGetters/main.cpp
+++ 26.Classes/26.5SettersAndGetters/main.cpp
@@ -1,35 +1,65 @@
-#include <iostream>
+ #include<iostream>
 
-using namespace std;
+ using namespace std;
 
-const double PI = 3.14;
-class Cylinder{
+ double PI {3.14};
+
+ class Cylinder{
     private:
-        double radius {2};
-        double height {4};
-
+        double priv_radius {10};
+        
+        double priv_height {10};
     public:
         Cylinder() = default;
-        Cylinder(double Base_radius, double Base_height){
-            radius = Base_radius;
-            height = Base_height;
+        Cylinder(double radius, double height){
+            priv_height = height;
+            priv_radius = radius;
         }
 
         double volume(){
-            return PI * radius * radius * height;
+            return PI * priv_height * priv_radius * priv_radius;
         }
 
-};
+        //seterss 
 
-int main(){
-    
+        double get_pri_radius(){
+            return priv_radius;
+        }
+
+        double get_pri_height(){
+            return priv_height;
+        }
+
+        void set_pri_height(double set_height){
+            priv_height = set_height;
+        }
+
+        void set_pri_radius(double set_raduis){
+            priv_radius = set_raduis;
+        }
+ };
+
+ int main(){
     Cylinder Cylinder1;
     Cylinder Cylinder2(1,2);
 
-    cout << "cylinder1 volume: " << Cylinder1.volume() << endl;
-    cout << "cylinder2 volume: " << Cylinder2.volume() << endl;
-    
-    
-    
+    cout << "priv_radius: " << Cylinder2.get_pri_radius() << endl;
+    cout << "priv_height: " << Cylinder2.get_pri_height() << endl;
+
+    Cylinder2.set_pri_height(5);
+    Cylinder2.set_pri_radius(5);
+
+    cout << "priv_radius: " << Cylinder2.get_pri_radius() << endl;
+    cout << "priv_height: " << Cylinder2.get_pri_height() << endl;
+
+    cout << "1priv_radius: " << Cylinder1.get_pri_radius() << endl;
+    cout << "1priv_height: " << Cylinder1.get_pri_height() << endl;
+
+    Cylinder1.set_pri_height(5);
+    Cylinder1.set_pri_radius(5);
+
+    cout << "1priv_radius: " << Cylinder1.get_pri_radius() << endl;
+    cout << "1priv_height: " << Cylinder1.get_pri_height() << endl;
+
     return 0;
-}
+ }
