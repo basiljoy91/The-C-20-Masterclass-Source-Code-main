179d9c1241143fc095095ff93caa745e3d297f4f
basiljoy91 <basiljoygreen@gmail.com>
2026-02-05 12:46:54 +0530

    stters and getters

--- 26.Classes/26.3Constructors/main.cpp
+++ 26.Classes/26.3Constructors/main.cpp
@@ -1,33 +1,16 @@
 #include<iostream>
 
-using namespace std;
-
-double PI = {3.14};
-
-class Cylinder{
-    private:
-
-        //member variables
-        double radius = {2};
-        double height = {5};
-    
-    public:
-        //constructor
-        Cylinder(){
-            radius = 1;
-            height = 2;
-        }
-
-        //member function or methords
-        double volume(){
-            return PI * radius * radius * height;
-        }  
-};
-
+int add(int a, int b){
+    return a+b;
+}
 int main(){
     
-    Cylinder cylinder1; //object
-    cout << "result: " << cylinder1.volume() << endl;
     
+    int a =3;
+    int b =3;
+    std::cout << add(a, b)<< std::endl;  
+     
+
     return 0;
 }
+
