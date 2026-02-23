49e1e692e73b43e495ae206d535190b34e396fd4
basiljoy91 <basiljoygreen@gmail.com>
2026-02-23 12:36:40 +0530

    class string_view

--- 26.Classes/26.10OrderOfConstructorDestructorCalls/main.cpp
+++ 26.Classes/26.10OrderOfConstructorDestructorCalls/main.cpp
@@ -10,4 +10,19 @@ class Dog{
         int* age {nullptr};
 
     public:
-       
+        Dog() = default;
+        Dog(string_view param_name, string_view param_breed, int param_age){
+            name = param_name;
+            breed = param_breed;
+            age = new int;
+            *age = param_age;
+
+            cout << "construted: " << name << endl;
+        }
+        ~Dog(){
+            delete age;
+            
+            cout << "destructed: " << name << endl;
+        }
+};
+
